0dee0ac68e98c68576e8e2259bb6be05df7e5182
Paulo Santos <paulo.santos@thoughtworks.com>
2020-07-06 16:15:23 -0300

    Initial Commit

---
+++ Movies-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Movies-SwiftUI
+//
+//  Created by Paulo Henrique dos Santos on 06/07/20.
+//  Copyright © 2020 Paulo Henrique dos Santos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
